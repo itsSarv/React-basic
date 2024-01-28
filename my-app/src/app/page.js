d31648455960d34efd057b76ce96ddc9c027a6c9
saurav tiwari <sauravtiwari145@gmail.com>
2024-01-28 18:30:56 +0545

    Learning Link tag of Next

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,13 +1,17 @@
+import Link from 'next/link'
+
 const app = ()=>{
   const navitem = ["Home", "About", "Contact"];
   return(
     <div>
+      
     <div style={{display:"flex"}}>
           {navitem.map((item, id)=>{
             return <div className="navitem">{item}</div>
           })}
     </div>
       <h1>Hello I'm Saurav</h1>
+      Please <Link href="/login"><button>Login</button></Link> to procced
     </div>
   )
 }
